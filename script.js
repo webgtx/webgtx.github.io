4e4e7ca96d1184b683a6a1ef7dadfba46b7cc214
webgtx <dxv1d@tuta.io>
2021-02-16 10:13:00 -0500

    Fixed about, responsive fonts added

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 const aboutLink = document.querySelector('.about__link');
 const aboutMain = document.querySelector('.about')
+const buttonBack = document.querySelector('.button-back')
 
 function linkClick() {
   aboutLink.addEventListener('click', () => {
       aboutMain.classList.add('about-active');
+      aboutMain.classList.remove('about-home');
+  });
+};
+
+function buttonClick() {
+  buttonBack.addEventListener('click', () => {
+      aboutMain.classList.remove('about-active');
+      aboutMain.classList.add('about-home');
   });
 }
 
 linkClick();
+buttonClick();
+// aboutLink.click();
