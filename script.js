07f5bafb048989a648289964d24f7d2e1561ad54
webgtx <dxv1d@tuta.io>
2021-04-25 01:09:37 +0300

    added first patch for portfolio page

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const aboutLink=document.querySelector(".about__link"),aboutMain=document.querySelector(".about"),buttonBack=document.querySelector(".button-back");function linkClick(){aboutLink.addEventListener("click",()=>{aboutMain.classList.add("about-active"),aboutMain.classList.remove("about-home")})}function buttonClick(){buttonBack.addEventListener("click",()=>{aboutMain.classList.remove("about-active"),aboutMain.classList.add("about-home")})}linkClick(),buttonClick();
+
+const aboutLink = document.querySelector('.about__link');
+const portfolioLink = document.querySelector('.portfolio__link')
+const aboutMain = document.querySelector('.about');
+const portfolioMain = document.querySelector('.portfolio');
+const buttonBack = document.querySelector('.button-back');
+
+function linkClick() {
+  aboutLink.addEventListener('click', () => {
+      aboutMain.classList.add('about-active');
+      aboutMain.classList.remove('about-home');
+  })
+  portfolioLink.addEventListener('click', () => {
+      portfolioMain.classList.add('portfolio-active');
+      portfolioMain.classList.remove('portfolio-home');
+  })
+}
+
+function buttonClick() {
+  buttonBack.addEventListener('click', () => {
+      aboutMain.classList.remove('about-active');
+      aboutMain.classList.add('about-home');
+  })
+}
+
+linkClick();
+buttonClick();
+portfolioLink.click();
--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,28 @@
 
 const aboutLink = document.querySelector('.about__link');
-const aboutMain = document.querySelector('.about')
-const buttonBack = document.querySelector('.button-back')
+const portfolioLink = document.querySelector('.portfolio__link')
+const aboutMain = document.querySelector('.about');
+const portfolioMain = document.querySelector('.portfolio');
+const buttonBack = document.querySelector('.button-back');
 
 function linkClick() {
   aboutLink.addEventListener('click', () => {
       aboutMain.classList.add('about-active');
       aboutMain.classList.remove('about-home');
-  });
-};
+  })
+  portfolioLink.addEventListener('click', () => {
+      portfolioMain.classList.add('portfolio-active');
+      portfolioMain.classList.remove('portfolio-home');
+  })
+}
 
 function buttonClick() {
   buttonBack.addEventListener('click', () => {
       aboutMain.classList.remove('about-active');
       aboutMain.classList.add('about-home');
-  });
+  })
 }
 
 linkClick();
 buttonClick();
-// aboutLink.click();
+portfolioLink.click();
